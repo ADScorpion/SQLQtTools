72a0518abb6e203925b902484d67ee3bf5ff84de
Sergey Skalozub <sSkalozub@GSServices.ru>
2025-06-11 11:54:38 +0300

    Version 0.0.5

--- SqlQtTools/__init__.py
+++ SqlQtTools/__init__.py
@@ -1,3 +1,11 @@
 DB_VERSION = 1
 DB_FILE = 'config.db'
 
+from SqlQtTools.general import tz_moscow, SQLTableType, SQLDAOType, DataSourceType
+from SqlQtTools.sql.make import init_db, update_db
+from SqlQtTools.sql.model import SysBaseTable, BaseTable, BaseExtTable
+from SqlQtTools.sql.dao import SysBaseDAO
+from SqlQtTools.qt.icons import BootstrapIcons, icon_provider
+from SqlQtTools.qt.model import SysBaseDS
+from SqlQtTools.qt.dialogs import SysBaseDialog
+from SqlQtTools.qt.forms import SysBaseMainWindow, SysBaseWidgetView
