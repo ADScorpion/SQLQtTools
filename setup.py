2a2fdead00091fa0a0f205c73c16aec4d829629e
Sergey Skalozub <sSkalozub@GSServices.ru>
2025-06-11 11:39:13 +0300

    Version 0.0.1. Add file setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+NAME = "SqlQtTools"
+VERSION = "0.0.1"
+REQUIRES = []
+
+setup(
+    name=NAME,
+    version=VERSION,
+    url='https://github.com/ADScorpion/SQLQtTool',
+    author='Sergey Skalozub',
+    description='SQLQtTool README',
+    keywords=[],
+    long_description='SQLQtTool long README',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=REQUIRES,
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Utilities'
+    ]
+)
