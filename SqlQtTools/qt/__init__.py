f212ea9dfaec51f1fa114ce8641cc496f9e9fcf0
Sergey Skalozub <sSkalozub@GSServices.ru>
2025-06-11 12:14:42 +0300

    Version 0.0.10

--- SqlQtTools/qt/__init__.py
+++ SqlQtTools/qt/__init__.py
@@ -1,8 +1,9 @@
 from typing import TypeVar
 
-from SqlQtTools.qt.icons import BootstrapIcons, icon_provider
 from SqlQtTools.qt.model import SysBaseDS
-from SqlQtTools.qt.dialogs import SysBaseDialog
-from SqlQtTools.qt.forms import SysBaseMainWindow, SysBaseWidgetView
 
 DataSourceType = TypeVar("DataSourceType", bound=SysBaseDS)
+
+from SqlQtTools.qt.icons import BootstrapIcons, icon_provider
+from SqlQtTools.qt.dialogs import SysBaseDialog
+from SqlQtTools.qt.forms import SysBaseMainWindow, SysBaseWidgetView
--- SqlQtTools/sql/__init__.py
+++ SqlQtTools/sql/__init__.py
@@ -1,9 +1,12 @@
 from typing import TypeVar
 
-from SqlQtTools.sql.make import init_db, update_db
-from SqlQtTools.sql.model import SysBaseTable, BaseTable, BaseExtTable
-from SqlQtTools.sql.dao import SysBaseDAO
-
+from SqlQtTools.sql.model import SysBaseTable
 
 SQLTableType = TypeVar("SQLTableType", bound=SysBaseTable)
+
+from SqlQtTools.sql.dao import SysBaseDAO
+
 SQLDAOType = TypeVar("SQLDAOType", bound=SysBaseDAO)
+
+from SqlQtTools.sql.make import init_db, update_db
+from SqlQtTools.sql.model import BaseTable, BaseExtTable
