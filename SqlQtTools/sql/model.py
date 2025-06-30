e7b14320f39f9314c69f97f966a27bfcf2500885
Sergey Skalozub <sSkalozub@GSServices.ru>
2025-06-30 17:39:06 +0300

    Version 0.1.3

--- SqlQtTools/sql/model.py
+++ SqlQtTools/sql/model.py
@@ -4,8 +4,7 @@ from datetime import datetime
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from sqlalchemy import String
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncAttrs
-from sqlalchemy.ext.declarative import AbstractConcreteBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 from SqlQtTools import DB_FILE
 
@@ -14,30 +13,34 @@ engine = create_async_engine(url=database_url)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-class SysBaseTable(AsyncAttrs, so.DeclarativeBase):
+class SysBaseTable(so.DeclarativeBase):
     """Базовое описание таблицы без базовых полей"""
+    __abstract__ = True
 
     @classmethod
     def grid_overview(cls):
         return []
 
 
-class SysBaseTableI(AbstractConcreteBase, SysBaseTable):
+class SysBaseTableI(SysBaseTable):
     """Базовое описание таблицы с полями Id"""
+    __abstract__ = True
 
     Id: so.Mapped[int] = so.mapped_column(primary_key=True, index=True, autoincrement=True, doc="Номер записи", info={'visible': False})
 
 
-class SysBaseTableIC(AbstractConcreteBase, SysBaseTable):
+class SysBaseTableIC(SysBaseTable):
     """Базовое описание таблицы с полями Id, CreatedBy, CreatedAt"""
+    __abstract__ = True
 
     Id: so.Mapped[int] = so.mapped_column(primary_key=True, index=True, autoincrement=True, doc="Номер записи", info={'visible': False})
     CreatedBy: so.Mapped[str] = so.mapped_column(String(100), default=getlogin(), doc="Создал", info={'visible': False})
     CreatedAt: so.Mapped[datetime] = so.mapped_column(default=sa.func.now(), doc="Создано", info={'visible': False})
 
 
-class SysBaseTableICM(AbstractConcreteBase, SysBaseTable):
+class SysBaseTableICM(SysBaseTable):
     """Базовое описание таблицы с полями Id, CreatedBy, CreatedAt, ModifiedBy, ModifiedAt"""
+    __abstract__ = True
 
     Id: so.Mapped[int] = so.mapped_column(primary_key=True, index=True, autoincrement=True, doc="Номер записи", info={'visible': False})
     CreatedBy: so.Mapped[str] = so.mapped_column(String(100), default=getlogin(), doc="Создал", info={'visible': False})
