ce6cde8778124e8f936d500efe8f1872982ab5ae
Sergey Skalozub <sSkalozub@GSServices.ru>
2025-07-02 15:57:36 +0300

    Version 0.1.11

--- SqlQtTools/qt/__init__.py
+++ SqlQtTools/qt/__init__.py
@@ -6,4 +6,4 @@ DataSourceMap = TypeVar("DataSourceMap", bound=SysBaseDS)
 
 from SqlQtTools.qt.icons import BootstrapIcons, icon_provider
 from SqlQtTools.qt.dialogs import SysBaseDialog
-from SqlQtTools.qt.forms import SysBaseMainWindow, SysBaseWidgetView
+from SqlQtTools.qt.forms import SysBaseMainWindow, SysBaseWidgetView, PySide6App
